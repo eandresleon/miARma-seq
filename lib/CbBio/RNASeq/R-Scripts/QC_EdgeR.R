0ff4600ba3098fdfaf076dd03518b5b12c4545d5
edu_andres <eleon-ibis@us.es>
2015-11-05 13:20:28 +0100

    Chaging QC output

--- lib/CbBio/RNASeq/R-Scripts/QC_EdgeR.R
+++ lib/CbBio/RNASeq/R-Scripts/QC_EdgeR.R
@@ -9,45 +9,45 @@
 
 #Printing the function information and  help message
 write("
-
-#########################################################################  
-  QC_EdgeR R function			                          	 									
-																		                                    
-	Created at Computational Biology and Bioinformatics Group (CbBio)	    
-	Institute of Biomedicine of Seville. IBIS (Spain)					            
-	Copyright (c) 2015 IBIS. All rights reserved.						              
-	mail : miarmaseq-devel@cbbio.es 								                      
-#########################################################################
-
-
-Get help typing QC_EdgeR_help()\n",stderr())
+      
+      #########################################################################  
+      QC_EdgeR R function			                          	 									
+      
+      Created at Computational Biology and Bioinformatics Group (CbBio)	    
+      Institute of Biomedicine of Seville. IBIS (Spain)					            
+      Copyright (c) 2015 IBIS. All rights reserved.						              
+      mail : miarmaseq-devel@cbbio.es 								                      
+      #########################################################################
+      
+      
+      Get help typing QC_EdgeR_help()\n",stderr())
 
 #Defining help function
 QC_EdgeR_help<-function(){
   write("
-QC_EdgeR is a R function which takes a tab file with the number of the reads from htseq-count analysis and a target file 
-with the experimental conditions of the samples to analyze the quality of the samples. For this purpose QC_EdgeR genrates a pdf 
-report with differents plots: boxplots and density plots with the distribution of the reads in each sample with 
-and without normalization, MDS and PCA plots and heatmaps of the samples with the more expressed elements(genes, miRNAs...)
-and between the samples. 
-  
-QC_EdgeR takes 10  arguments:
-  
-Mandatory arguments:
-  [projectdir] Path of the directory where will be saved the QC report.
-  [dir] the path of the directory which contains the files. This directory will be configured as working directory for R
-  [file] name of the tab file which contains the number of reads from the htseq analysis
-  [targetfile] Path of the tabulated file which contains the experimental condiction of each sample. 
-  First column must contain the names to be used to the plots, and the next columns the condition of each factor. 
-  QC_EdgeR works with 1 or 2 factors.
-  [label] Name of the experiment to appear in the title of the plots and in the name of the pdf file results
-  [filter] This value refers to filter processing in the reads (Should be \"yes\" or \"no\").
-  
-Optional arguments:
-  [cpmvalue] Cutoff for the counts per million value to be used in filter processing (1 cpm by default).
-  [repthreshold] Number of replicates that have to contains at least a defined number of reads per million to perform the filter (2 replicates by default)", stderr())
+        QC_EdgeR is a R function which takes a tab file with the number of the reads from htseq-count analysis and a target file 
+        with the experimental conditions of the samples to analyze the quality of the samples. For this purpose QC_EdgeR genrates a pdf 
+        report with differents plots: boxplots and density plots with the distribution of the reads in each sample with 
+        and without normalization, MDS and PCA plots and heatmaps of the samples with the more expressed elements(genes, miRNAs...)
+        and between the samples. 
+        
+        QC_EdgeR takes 10  arguments:
+        
+        Mandatory arguments:
+        [projectdir] Path of the directory where will be saved the QC report.
+        [dir] the path of the directory which contains the files. This directory will be configured as working directory for R
+        [file] name of the tab file which contains the number of reads from the htseq analysis
+        [targetfile] Path of the tabulated file which contains the experimental condiction of each sample. 
+        First column must contain the names to be used to the plots, and the next columns the condition of each factor. 
+        QC_EdgeR works with 1 or 2 factors.
+        [label] Name of the experiment to appear in the title of the plots and in the name of the pdf file results
+        [filter] This value refers to filter processing in the reads (Should be \"yes\" or \"no\").
+        
+        Optional arguments:
+        [cpmvalue] Cutoff for the counts per million value to be used in filter processing (1 cpm by default).
+        [repthreshold] Number of replicates that have to contains at least a defined number of reads per million to perform the filter (2 replicates by default)", stderr())
 }
-  
+
 QC_EdgeR<-function(projectdir,dir,file,targetfile,label,filter, cpmvalue=1, repthreshold=2,normethod="TMM", bcvvalue=0.4){
   
   #Checking the mandatory parameters
@@ -61,17 +61,17 @@ QC_EdgeR<-function(projectdir,dir,file,targetfile,label,filter, cpmvalue=1, rept
   #########################################################################
   
   #installing need packages
-	list.of.packages <- c("ggplot2", "Rcpp","edgeR","NOISeq","gplots","lattice","genefilter","RColorBrewer")
-	new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-	if(length(new.packages)){
-		
-		#install.packages(new.packages)
-		source("http://bioconductor.org/biocLite.R")
-		biocLite(new.packages,
-			suppressUpdates=FALSE,
-            suppressAutoUpdate=FALSE,
-            ask=TRUE)
-	}
+  list.of.packages <- c("ggplot2", "Rcpp","edgeR","NOISeq","gplots","lattice","genefilter","RColorBrewer")
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)){
+    
+    #install.packages(new.packages)
+    source("http://bioconductor.org/biocLite.R")
+    biocLite(new.packages,
+             suppressUpdates=FALSE,
+             suppressAutoUpdate=FALSE,
+             ask=TRUE)
+  }
   
   #Loading the needed packagge
   require(edgeR)
@@ -103,22 +103,22 @@ QC_EdgeR<-function(projectdir,dir,file,targetfile,label,filter, cpmvalue=1, rept
   #########################################################################
   #2-EDGER ANALYSIS-BOXPLOT, DENSITY PLOT AND CLUSTERING
   #########################################################################
-
+  
   #Importing targets
   targets<-readTargets(targetfile)
   options(warn=-1)
   #Checking the dimensions of the targets
   #For 1 dimension target file must contain 2 columns: names and condition and for 2 dimensions must contain 3: names, condition 1 and condition 2
-  if(length(colnames(targets))==2){
-    group<-factor(targets[,2])
-  }else if(length(colnames(targets))==3){
-    group<-factor(paste(targets[,2],targets[,3],sep="_"))
+  if(length(colnames(targets))==3){
+    group<-factor(targets[,3])
+  }else if(length(colnames(targets))==4){
+    group<-factor(paste(targets[,3],targets[,2],sep="_"))
   }else{
     stop("Target file has an incorrect format. Please see documentation to get information of the correct format of this file")
   }
   #Merging the conditions 
   cbind(targets,group=group)
- 
+  
   #Obtaining the number of replicates
   df<- cbind(targets,group=group)
   levelsnames<-levels(group)
@@ -162,7 +162,7 @@ QC_EdgeR<-function(projectdir,dir,file,targetfile,label,filter, cpmvalue=1, rept
     }
   }
   #Obtaining the names of the samples
-  samplenames<-as.character(targets[,1])
+  samplenames<-as.character(targets[,2])
   #Boxplot of the samples
   boxplot(log2(data), main=paste("Boxplot of ",label," samples",sep=""),las=2, names=samplenames, ylab="log2(counts)", xlab="")
   #Normalization of the samples
@@ -186,7 +186,7 @@ QC_EdgeR<-function(projectdir,dir,file,targetfile,label,filter, cpmvalue=1, rept
   for (a in 2:length(colnames(data))){
     number<-check_parameter(data[,a])
     if(number>1){
-  	  lines(density(log10(data[,a])),col=boxcol[a], lwd=1.5, lty=lineformat[a])
+      lines(density(log10(data[,a])),col=boxcol[a], lwd=1.5, lty=lineformat[a])
     }
   }
   legend("topright", samplenames, col=boxcol, lty=lineformat, lwd=4, cex=0.8)
@@ -194,10 +194,10 @@ QC_EdgeR<-function(projectdir,dir,file,targetfile,label,filter, cpmvalue=1, rept
   #Density plot of normalized counts data
   plot(density(log10(dgenorm$counts[,1])),col=boxcol[1],ylim=c(0,0.5), main=paste("Density plot of normalized and filtered number of reads of ", label, sep=""), lwd=1.5, xlab="log10(counts)")
   for (a in 2:length(colnames(data))){
-      number<-check_parameter(dgenorm$counts[,a])
-      if(number>1){
-	      lines(density(log10(dgenorm$counts[,a])),col=boxcol[a], lwd=1.5, lty=lineformat[a])
-	  }
+    number<-check_parameter(dgenorm$counts[,a])
+    if(number>1){
+      lines(density(log10(dgenorm$counts[,a])),col=boxcol[a], lwd=1.5, lty=lineformat[a])
+    }
   }
   legend("topright", samplenames, col=boxcol, lty=lineformat, lwd=4, cex=0.8)
   
@@ -232,7 +232,7 @@ QC_EdgeR<-function(projectdir,dir,file,targetfile,label,filter, cpmvalue=1, rept
   #########################################################################
   #4-HEATMAP PLOT
   #########################################################################
-    
+  
   rsd <- rowSds(as.matrix(dgenorm))
   sel <- order(rsd, decreasing=TRUE)[1:250]
   #hmcol= colorRampPalette(brewer.pal(9, "YlGnBu"))(100) 
